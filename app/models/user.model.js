5cf539c4faad546ce1f1e576a61dc8e628547ddc
KIQFerns <52255280+KIQFerns@users.noreply.github.com>
2025-04-26 09:19:46 -0300

    feat: melhorando o model para se adequar ao solicitado

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,13 +1,26 @@
 module.exports = (sequelize, Sequelize) => {
   const User = sequelize.define("user", {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
     name: {
       type: Sequelize.STRING,
+      allowNull: false,
     },
-    age: {
+    email: {
       type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
     },
     active: {
       type: Sequelize.BOOLEAN,
+      defaultValue: true,
     },
   });
 
