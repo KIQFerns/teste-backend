2d36dc8fbaf31b6019ce2e77027d97a0ce15f11f
KIQFerns <52255280+KIQFerns@users.noreply.github.com>
2025-04-26 09:53:28 -0300

    feat: add controllers

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,28 +1,39 @@
 module.exports = (sequelize, Sequelize) => {
-  const User = sequelize.define("user", {
-    id: {
-      type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4,
-      primaryKey: true,
-      allowNull: false,
-    },
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
+  const User = sequelize.define(
+    "user",
+    {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      active: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
       },
     },
-    active: {
-      type: Sequelize.BOOLEAN,
-      defaultValue: true,
-    },
-  });
+    {
+      timestamps: true,
+      createdAt: "createdAt",
+    }
+  );
 
   return User;
 };
